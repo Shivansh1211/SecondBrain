60a31ab6a652c295119a765ff1f71d9c14993777
Shivansh1211 <shivanshkumar1112@gmail.com>
2026-03-11 02:38:32 +0530

    Initial Spring Boot Project Setup with PostgreSQL

---
+++ SecondBrain/src/main/java/com/secondbrain/ai/SecondBrain/SecondBrainApplication.java
@@ -0,0 +1,13 @@
+package com.secondbrain.ai.SecondBrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondBrainApplication.class, args);
+	}
+
+}
---
+++ SecondBrain/src/test/java/com/secondbrain/ai/SecondBrain/SecondBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondbrain.ai.SecondBrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

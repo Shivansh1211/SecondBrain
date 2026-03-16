27681d6716b9aa5df66c5556f3292eda2482fba4
Shivansh1211 <shivanshkumar1112@gmail.com>
2026-03-17 03:15:41 +0530

    chore:formatting fix

--- SecondBrain/src/main/java/com/secondbrain/ai/SecondBrain/repository/DocumentRepository.java
+++ SecondBrain/src/main/java/com/secondbrain/ai/SecondBrain/repository/DocumentRepository.java
@@ -1,4 +1,9 @@
 package com.secondbrain.ai.SecondBrain.repository;
 
-public interface DocumentRepository {
-}
+import com.secondbrain.ai.SecondBrain.entity.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+    }
+

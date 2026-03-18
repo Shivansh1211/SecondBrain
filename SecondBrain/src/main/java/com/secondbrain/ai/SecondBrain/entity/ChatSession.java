f8bd059fbb71f7f7160f0419d0e2443d562bcd14
Shivansh1211 <shivanshkumar1112@gmail.com>
2026-03-18 23:55:54 +0530

    chore: removed unused import

--- SecondBrain/src/main/java/com/secondbrain/ai/SecondBrain/entity/ChatSession.java
+++ SecondBrain/src/main/java/com/secondbrain/ai/SecondBrain/entity/ChatSession.java
@@ -3,7 +3,6 @@ package com.secondbrain.ai.SecondBrain.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 
-import javax.swing.text.Document;
 
 @Entity
 public class ChatSession extends BaseEntity {

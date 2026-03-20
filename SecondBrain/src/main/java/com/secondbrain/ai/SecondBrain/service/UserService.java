37643dbaf470a9ceea97b15a0373c6a49bb74783
Shivansh1211 <shivanshkumar1112@gmail.com>
2026-03-21 02:22:00 +0530

    feat:add UserService Interface

---
+++ SecondBrain/src/main/java/com/secondbrain/ai/SecondBrain/service/UserService.java
@@ -0,0 +1,13 @@
+package com.secondbrain.ai.SecondBrain.service;
+import com.secondbrain.ai.SecondBrain.dto.user.RegisterRequest;
+import com.secondbrain.ai.SecondBrain.dto.user.UserResponse;
+
+
+import java.util.List;
+public interface UserService {
+    UserResponse register(RegisterRequest request);
+    UserResponse findById(Long id);
+    List<UserResponse> findAll();
+    UserResponse update(Long id, RegisterRequest request);
+    void delete(Long id);
+}
